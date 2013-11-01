3ac32346d6da47a7e6049c99c66d540e8c4c8a3f
everrise-narumi-mizukoshi <narumi.mizukoshi@ever-rise.co.jp>
2013-11-01 17:18:01 +0900

    改行コードの変更、日本語排除

--- openresty/attributes/default.rb
+++ openresty/attributes/default.rb
@@ -1,11 +1,13 @@
-# 作業用ディレクトリ 
+# work dir.
 default[:openresty][:work_dir] = '/etc/openresty'
 default[:openresty][:log_dir] = '/var/log/openresty'
 default[:openresty][:user] = 'openresty'
-# ソースコードの URL 
+# source code URL 
 default[:openresty][:source_ver_num] = '1.4.3.1'
 default[:openresty][:source_url_path] = "http://openresty.org/download/"
 default[:openresty][:source_file_name] = 'ngx_openresty-#{default[:openresty][:source_ver_num]}.tar.gz'
 
-# redis-server のインストールパス
+# redis-server install path
 #default[:openresty][:server_install_path] = '/usr/local/bin/openresty-server'
+
+
