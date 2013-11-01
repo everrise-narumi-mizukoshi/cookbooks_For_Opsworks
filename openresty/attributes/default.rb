d594137d16e55e01d3456b1f2ec1931a49286936
everrise-narumi-mizukoshi <narumi.mizukoshi@ever-rise.co.jp>
2013-11-01 16:34:09 +0900

    attributes/default.rb 修正

--- openresty/attributes/default.rb
+++ openresty/attributes/default.rb
@@ -4,8 +4,8 @@ default[:openresty][:log_dir] = '/var/log/openresty'
 
 # ソースコードの URL 
 default[:openresty][:source_ver_num] = '1.4.3.1'
-default[:openresty][:source_url_path] = "http://agentzh.org/misc/nginx/"
-default[:openresty][:source_file_name] = 'ngx_openresty-#{default['openresty']['source_ver_num']}.tar.gz'
+default[:openresty][:source_url_path] = 'http://agentzh.org/misc/nginx/'
+default[:openresty][:source_file_name] = 'ngx_openresty-#{default[:openresty][:source_ver_num]}.tar.gz'
 
 # redis-server のインストールパス
 default[:openresty][:server_install_path] = '/usr/local/bin/openresty-server'
