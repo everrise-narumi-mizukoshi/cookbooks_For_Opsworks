41cebfa404d4c63839598c9567e24545870b02a8
everrise-narumi-mizukoshi <narumi.mizukoshi@ever-rise.co.jp>
2013-11-01 14:39:12 +0900

    compile error 部分修正

--- openresty/attributes/default.rb
+++ openresty/attributes/default.rb
@@ -4,7 +4,7 @@ default[:openresty][:log_dir] = '/var/log/openresty'
 
 # ソースコードの URL 
 default[:openresty][:source_ver_num] = '1.4.3.1'
-default[:openresty][:source_url_path] = 'http://agentzh.org/misc/nginx/'
+default[:openresty][:source_url_path] = "http://agentzh.org/misc/nginx/"
 default[:openresty][:source_file_name] = 'ngx_openresty-#{default['openresty']['source_ver_num']}.tar.gz'
 
 # redis-server のインストールパス
