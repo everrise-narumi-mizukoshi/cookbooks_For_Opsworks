b73b43ab9447fdbeec5f15087c912769e61ac565
everrise-narumi-mizukoshi <narumi.mizukoshi@ever-rise.co.jp>
2013-11-01 17:27:22 +0900

    ””を’ ’に変更

--- openresty/attributes/default.rb
+++ openresty/attributes/default.rb
@@ -2,9 +2,10 @@
 default[:openresty][:work_dir] = '/etc/openresty'
 default[:openresty][:log_dir] = '/var/log/openresty'
 default[:openresty][:user] = 'openresty'
+
 # source code URL 
 default[:openresty][:source_ver_num] = '1.4.3.1'
-default[:openresty][:source_url_path] = "http://openresty.org/download/"
+default[:openresty][:source_url_path] = 'http://openresty.org/download/'
 default[:openresty][:source_file_name] = 'ngx_openresty-#{default[:openresty][:source_ver_num]}.tar.gz'
 
 # redis-server install path
