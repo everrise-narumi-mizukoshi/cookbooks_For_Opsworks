e61dff9ed7cbd3ade3789d13b3a3047b881ec254
everrise-narumi-mizukoshi <narumi.mizukoshi@ever-rise.co.jp>
2013-11-05 13:22:07 +0900

    展開ファイルの名前修正

--- openresty/attributes/default.rb
+++ openresty/attributes/default.rb
@@ -7,6 +7,7 @@ default[:openresty][:user] = 'openresty'
 default[:openresty][:ver_num] = '1.4.3.1'
 default[:openresty][:url_path] = 'http://openresty.org/download/'
 default[:openresty][:file_name] = 'ngx_openresty-1.4.3.1.tar.gz'
+default[:openresty][:new_file_name] = 'openrestyngx_openresty-1.4.3.1.tar.gz'
 
 # redis-server install path
 #default[:openresty][:server_install_path] = '/usr/local/bin/openresty-server'
