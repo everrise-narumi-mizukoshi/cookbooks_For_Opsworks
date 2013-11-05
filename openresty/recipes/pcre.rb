983ecb14feabe9dd573f4c1a032ff8c8a24fa519
everrise-narumi-mizukoshi <narumi.mizukoshi@ever-rise.co.jp>
2013-11-05 19:21:42 +0900

    pcreのバージョン変更

--- openresty/recipes/pcre.rb
+++ openresty/recipes/pcre.rb
@@ -4,13 +4,12 @@
 #
 
 package "pcre" do
+  version  "8.33"
   action   :install
-  source   "/var/package/pcre-6.6-6.el5_6.1.i386.rpm"
-  provider Chef::Provider::Package::Rpm
 end
 
 package "pcre-devel" do
   action   :install
-  source   "/var/package/pcre-devel-6.6-6.el5_6.1.i386.rpm"
-  provider Chef::Provider::Package::Rpm
+#  source   "/var/package/pcre-devel-6.6-6.el5_6.1.i386.rpm"
+#  provider Chef::Provider::Package::Rpm
 end
