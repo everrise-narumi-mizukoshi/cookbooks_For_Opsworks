5dd54b3145b7add32d36c6b0501f7b93a37b0aa3
everrise-narumi-mizukoshi <narumi.mizukoshi@ever-rise.co.jp>
2013-11-05 18:01:03 +0900

    エラー周辺の修正(pcre)

--- openresty/recipes/pcre.rb
+++ openresty/recipes/pcre.rb
@@ -19,6 +19,15 @@ pcre_src_filename = ::File.basename(node['pcre']['file'])
 pcre_src_filepath = "#{Chef::Config['file_cache_path']}/#{pcre_src_filename}"
 pcre_extract_path = "#{Chef::Config['file_cache_path']}/openresty-#{node['pcre']['version']}"
 
+node.run_state['pcre_configure_flags'] = [
+  '--enable-pcregrep-libz',
+  '--enable-pcregrep-libbz2',
+  '--enable-newline-is-anycrlf'
+]
+
+node.run_state['pcre_configure_flags'] |= [ '--enable-unicode-properties', '--enable-utf' ] if node['pcre']['enable_utf8']
+node.run_state['pcre_configure_flags'] |= [ '--enable-jit' ] if node['pcre']['enable_jit']
+
 # create directory 
 directory '/etc/pcre/' do
   owner 'root'
