131f955d8d6463649a40dee4b6ffc4e1b0ab7bc7
everrise-narumi-mizukoshi <narumi.mizukoshi@ever-rise.co.jp>
2013-11-05 11:32:33 +0900

    改行コードの変更、Bush cwdの修正

--- openresty/recipes/openresty_install.rb
+++ openresty/recipes/openresty_install.rb
@@ -3,7 +3,11 @@
 # Recipe:: default
 #
 #
-# 作業用ディレクトリの作成 
+
+openresty_src_filename = ::File.basename(node[:openresty][:file_name])
+openresty_src_filepath = "#{Chef::Config['file_cache_path']}/#{openresty_src_filename}"
+
+# create directory 
 directory '/etc/openresty' do
   owner 'root'
   group 'root'
@@ -17,16 +21,16 @@ end
 #  action :create
 #end
 
-# 最新のソースコードを取得 
+# get the ratest source-code 
 remote_file node[:openresty][:work_dir] + node[:openresty][:file_name] do
   source node[:openresty][:url_path] + node[:openresty][:file_name]
 end
-
+  
 # ソースコードのアーカイブを展開して make && make test && make install
 bash 'install openresty' do
   user 'root'
-  cwd ::File.dirname(node[:openresty][:url_path] + node[:openresty][:file_name])
-  code <<-EOH
+    cwd ::File.dirname(node[:openresty][:url_path] + node[:openresty][:file_name])
+    code <<-EOH
     tar xzf #{node[:openresty][:file_name]}
     cd #{::File.basename(node[:openresty][:file_name], '.tar.gz')} 
     ./configure --with-luajit
@@ -35,8 +39,9 @@ bash 'install openresty' do
   EOH
 end
 
-# サービスを起動する
+# start service
 service 'openresty' do
   supports :status => true, :restart => true, :reload => true
   action :start
 end
+    
